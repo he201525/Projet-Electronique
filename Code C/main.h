e47b81c40f88fa3ec27c5f281de567b2c165c5d3
Thibaut Hermant <hermantthibaut@gmail.com>
2019-05-19 18:15:00 +0200

    Update main.h

--- Code C/main.h
+++ Code C/main.h
@@ -1,4 +1,4 @@
 #include <18F458.h>
 #device ADC=10
 #use delay(crystal=20MHz)
-#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
+#use rs232(UART1,baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=COM2)
